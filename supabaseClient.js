cda9d79f47acd4e8029201674ac5e9681e0d6364
minegocios <minegociosinteligentes@gmail.com>
2025-02-25 21:17:35 -0300

    Update supabaseClient.js

--- supabaseClient.js
+++ supabaseClient.js
@@ -1,12 +1,15 @@
 const { createClient } = require('@supabase/supabase-js');
 
+// Lê as variáveis de ambiente
 const SUPABASE_URL = process.env.SUPABASE_URL;
 const SUPABASE_KEY = process.env.SUPABASE_KEY;
 
+// Verifica se as variáveis estão definidas
 if (!SUPABASE_URL || !SUPABASE_KEY) {
-  throw new Error('SUPABASE_URL and SUPABASE_KEY must be set in the .env file.');
+  throw new Error('SUPABASE_URL and SUPABASE_KEY must be set in the Environment Variables.');
 }
 
+// Cria o cliente Supabase
 const supabase = createClient(SUPABASE_URL, SUPABASE_KEY);
 
 module.exports = supabase;
